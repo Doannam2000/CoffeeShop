7d9542184b55123d5b110d2783801f31dd1edba9
DoanNam2000 <doanduynam2000@gmail.com>
2022-03-28 21:37:45 +0700

    update chart ui

--- app/src/main/java/com/ddwan/coffeeshop/adapter/BillAdapter.kt
+++ app/src/main/java/com/ddwan/coffeeshop/adapter/BillAdapter.kt
@@ -1,6 +1,8 @@
 package com.ddwan.coffeeshop.adapter
 
 import android.annotation.SuppressLint
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -11,7 +13,7 @@ import androidx.recyclerview.widget.RecyclerView
 import com.ddwan.coffeeshop.R
 import com.ddwan.coffeeshop.model.BillInfo
 
-class BillAdapter(var list:ArrayList<BillInfo>):RecyclerView.Adapter<BillAdapter.ViewHolder>() {
+class BillAdapter(var list: ArrayList<BillInfo>) : RecyclerView.Adapter<BillAdapter.ViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BillAdapter.ViewHolder {
         var view: View =
             LayoutInflater.from(parent.context).inflate(R.layout.item_bill_layout, parent, false)
@@ -25,16 +27,45 @@ class BillAdapter(var list:ArrayList<BillInfo>):RecyclerView.Adapter<BillAdapter
     override fun getItemCount(): Int {
         return list.size
     }
-    inner class ViewHolder(itemView: View):RecyclerView.ViewHolder(itemView){
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         private var image: ImageView = itemView.findViewById(R.id.imageFood)
         private var name: TextView = itemView.findViewById(R.id.nameFood)
         private var price: TextView = itemView.findViewById(R.id.price)
         private var count: EditText = itemView.findViewById(R.id.countFood)
+        private var imagePlus: ImageView = itemView.findViewById(R.id.add)
+        private var imageMinus: ImageView = itemView.findViewById(R.id.minus)
+
         @SuppressLint("SetTextI18n")
         fun setData() {
+            var i = list[adapterPosition].count
             name.text = list[adapterPosition].foodId.toString()
-            count.setText(list[adapterPosition].count.toString())
-            price.text = (list[adapterPosition].price*list[adapterPosition].count).toString() + "đ"
+            count.setText(i.toString())
+            price.text =
+                (list[adapterPosition].price * list[adapterPosition].count).toString() + "đ"
+            imagePlus.setOnClickListener {
+                i++
+                count.setText(i.toString())
+            }
+            imageMinus.setOnClickListener {
+                if (i - 1 != 0) {
+                    i--
+                    count.setText(i.toString())
+                }
+            }
+            count.addTextChangedListener(object : TextWatcher {
+                override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                }
+
+                override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                }
+
+                override fun afterTextChanged(p0: Editable?) {
+                    price.text = (list[adapterPosition].price * count.text.toString()
+                        .toInt()).toString() + "đ"
+                }
+            })
+
         }
     }
 }
--- app/src/main/java/com/ddwan/coffeeshop/fragment/ChartFragment.kt
+++ app/src/main/java/com/ddwan/coffeeshop/fragment/ChartFragment.kt
@@ -1,11 +1,17 @@
 package com.ddwan.coffeeshop.fragment
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.ddwan.coffeeshop.R
+import com.github.mikephil.charting.charts.BarChart
+import com.github.mikephil.charting.data.BarData
+import com.github.mikephil.charting.data.BarDataSet
+import com.github.mikephil.charting.data.BarEntry
+import com.github.mikephil.charting.utils.ColorTemplate
 
 
 class ChartFragment : Fragment() {
@@ -15,7 +21,26 @@ class ChartFragment : Fragment() {
         savedInstanceState: Bundle?,
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_chart, container, false)
+        val view = inflater.inflate(R.layout.fragment_chart, container, false)
+        val barchart: BarChart = view.findViewById(R.id.chart)
+        val list = arrayListOf<BarEntry>(BarEntry(2016f, 560f),
+            BarEntry(2017f, 202f),
+            BarEntry(2018f, 300f),
+            BarEntry(2019f, 1200f),
+            BarEntry(2020f, 770f))
+        val barDataset = BarDataSet(list,"Doanh thu")
+        barDataset.colors = mutableListOf(Color.BLUE,Color.CYAN,Color.RED,Color.GRAY,Color.GREEN)
+        barDataset.valueTextColor = Color.BLACK
+        barDataset.valueTextSize = 16f
+
+        val barData = BarData(barDataset)
+
+        barchart.setFitBars(true)
+        barchart.data = barData
+        barchart.animateY(2000)
+
+
+        return view
     }
 
 }
